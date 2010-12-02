176f4f048b4abebe30f8a0c9ffb620e3794b1803
tschaub <tim.schaub@gmail.com>
2010-12-02 15:22:41 -0700

    Application skeleton.

---
+++ app/config.js
@@ -0,0 +1,55 @@
+// map url patterns to exported JSGI app functions
+var urls = [
+    [(/^\/proxy/), require("./proxy").app]
+];
+
+// debug mode loads unminified scripts
+// assumes markup pulls in scripts under the path /servlet_name/script/
+if (java.lang.System.getProperty("READYGO_DEBUG")) {
+    var fs = require("fs");
+    var config = fs.normal(fs.join(module.directory, "..", "buildjs.cfg"));
+    urls.push(
+        [(/^\/script(\/.*)/), require("./autoloader").App(config)]
+    );    
+}
+
+exports.urls = urls;
+
+// redirect requests without a trailing slash
+// Jetty does this automatically for /servlet_name, Tomcat does not
+function slash(config) {
+    return function(app) {
+        return function(request) {
+            var response;
+            var servletRequest = request.env.servletRequest;
+            var pathInfo = servletRequest.getPathInfo();
+            if (pathInfo === "/") {
+                var uri = servletRequest.getRequestURI();
+                if (uri.charAt(uri.length-1) !== "/") {
+                    var location = servletRequest.getScheme() + "://" + 
+                        servletRequest.getServerName() + ":" + servletRequest.getServerPort() + 
+                        uri + "/";
+                    return {
+                        status: 301,
+                        headers: {"Location": location},
+                        body: []
+                    };
+                }
+            }
+            return app(request);
+        }
+    }
+}
+
+exports.middleware = [
+    slash(),
+    require("ringo/middleware/gzip").middleware,
+    require("ringo/middleware/static").middleware({base: module.resolve("static"), index: "index.html"}),
+    require("ringo/middleware/error").middleware,
+    require("ringo/middleware/notfound").middleware
+];
+
+exports.app = require("ringo/webapp").handleRequest;
+
+exports.charset = "UTF-8";
+exports.contentType = "text/html";
