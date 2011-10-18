c6983af5e626095a0a3f3302f0a70fd22a7bd5ef
tschaub <tim.schaub@gmail.com>
2011-10-18 08:53:23 -0600

    Trailing slash fix for Tomcat.

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 var {Application} = require("stick");
 
-var app = exports.app = Application();
+var app = Application();
 app.configure("notfound", "error", "static", "params", "mount");
 app.static(module.resolve("static"), "index.html");
 app.mount("/proxy", require("./proxy").app);
@@ -23,6 +23,31 @@ if (java.lang.System.getProperty("app.debug")) {
         app.mount("/geoserver/", require("./proxy").pass({url: geoserver, preserveHost: true}));
     }
 }
+
+// Redirect requests for servlet name without a trailing slash.
+// Jetty does this automatically for /servlet_name, Tomcat does not.
+function slash(app) {
+    return function(request) {
+        var servletRequest = request.env.servletRequest;
+        var pathInfo = servletRequest.getPathInfo();
+        if (pathInfo === "/") {
+            var uri = servletRequest.getRequestURI();
+            if (uri.charAt(uri.length-1) !== "/") {
+                var location = servletRequest.getScheme() + "://" + 
+                    servletRequest.getServerName() + ":" + servletRequest.getServerPort() + 
+                    uri + "/";
+                return {
+                    status: 301,
+                    headers: {"Location": location},
+                    body: []
+                };
+            }
+        }
+        return app(request);
+    };
+}
+
+exports.app = slash(app);
 
 // main script to start application
 if (require.main === module) {
